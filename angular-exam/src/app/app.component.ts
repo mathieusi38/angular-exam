1c89a538811a2410747df5440f0559d7c2f6f1ee
mathieusi38 <mathieusi38@gmail.com>
2023-05-10 12:01:40 +0200

    init projet

---
+++ angular-exam/src/app/app.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+import { FormControl, FormBuilder, Validators , FormGroup } from '@angular/forms';
+import {TranslateService} from '@ngx-translate/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  TransLang=["en","fr"];
+  selectLang:string = "";
+  title: FormControl;
+  description: FormControl;
+  form: FormGroup = new FormGroup({});
+ // title: FormControl = new FormControl("");
+
+  constructor(public translate: TranslateService, private fb: FormBuilder) {
+
+      // this language will be used as a fallback when a translation isn't found in the current language
+    if (navigator.language.split('-')[0]  == "fr") {
+      translate.use('fr');
+    } else {
+      translate.use(translate.getDefaultLang());
+    }
+
+    this.title = new FormControl("",Validators.required);
+    this.description = new FormControl("",Validators.required);
+  }
+
+  setTransLanguage(){
+    //this.TransLang= this.translate.getLangs();
+    this.translate.use(this.selectLang);
+    }
+  
+}
---
+++ angular-exam/src/app/app.module.ts
@@ -0,0 +1,37 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import {TranslateModule, TranslateLoader} from '@ngx-translate/core';
+import {HttpClientModule, HttpClient} from '@angular/common/http';
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule  } from '@angular/forms';
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    HttpClientModule,
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    TranslateModule.forRoot({
+      defaultLanguage: 'en',
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+    }
+    }),
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
